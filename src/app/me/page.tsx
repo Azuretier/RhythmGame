2428730e6d47b9ddb83ddc80cbb4d0c875b2cf1e
Azureit <d.f.18th@gmail.com>
2024-10-18 04:34:29 +0900

    c

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,20 +1,11 @@
-'use client'
-
+import { FadeUpDiv, FadeUpStagger } from '@/components/animation';
 import Image from "next/image";
-import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
-        <motion.div
-            initial={{ opacity: 0, y: 100 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{
-              duration: 0.5,      // アニメーションの時間
-              ease: 'easeOut',    // イージングで動きを調整
-            }}
-            className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
-        >
+    <FadeUpStagger>
+        <FadeUpDiv className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
+        
             <div className="shrink-0">
                 <Image className="size-24" src="/azure.webp" width={800} height={800} quality={75} alt="Azure Logo"/>
             </div>
@@ -22,7 +13,8 @@ export default function Home() {
                 <div className="text-xl font-medium text-black">Azure</div>
                 <p className="text-slate-500">You have a new message!</p>
             </div>
-        </motion.div>
-    </div>
+        
+        </FadeUpDiv>
+    </FadeUpStagger>
   );
 }
