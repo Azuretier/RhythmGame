f17c709a058c458fe1946d537be97096370c0246
Azureit <d.f.18th@gmail.com>
2024-10-18 05:10:00 +0900

    c

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -11,29 +11,29 @@ export default function Home() {
         setIsLoaded(true);
     }, []); // 初回レンダリング時のみ実行される
     
-  return (
-    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
-        <AnimatePresence>
-        <motion.div
-            layout
-            key={Math.random()}
-            initial={{ opacity: 0, y: 100 }}
-            animate={isLoaded ? { opacity: 1, y: 0 }}
-            exit={{ opacity: 0 }}
-            transition={{
-              duration: 1
-            }}
-            className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
-        >
-            <div className="shrink-0">
-                <Image className="size-24" src="/azure.webp" width={800} height={800} quality={75} alt="Azure Logo"/>
-            </div>
-            <div>
-                <div className="text-xl font-medium text-black">Azure</div>
-                <p className="text-slate-500">You have a new message!</p>
-            </div>
-        </motion.div>
-        </AnimatePresence>
-    </div>
-  );
+    return (
+        <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
+            <AnimatePresence>
+            <motion.div
+                layout
+                key={Math.random()}
+                initial={{ opacity: 0, y: 100 }}
+                animate={isLoaded ? { opacity: 1, y: 0 }}
+                exit={{ opacity: 0 }}
+                transition={{
+                duration: 1
+                }}
+                className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
+            >
+                <div className="shrink-0">
+                    <Image className="size-24" src="/azure.webp" width={800} height={800} quality={75} alt="Azure Logo"/>
+                </div>
+                <div>
+                    <div className="text-xl font-medium text-black">Azure</div>
+                    <p className="text-slate-500">You have a new message!</p>
+                </div>
+            </motion.div>
+            </AnimatePresence>
+        </div>
+    );
 }
