26ab7b5f1ac5dcde8a7c854ee32b880ee39304f9
Azureit <d.f.18th@gmail.com>
2024-10-19 07:49:57 +0900

    c

--- src/components/org/c.tsx
+++ src/components/org/c.tsx
@@ -1,6 +1,9 @@
 const Konyuuta = () => {
     return (
-        <main>
+        // items-center = vertical
+        // justify-center = horizontal
+
+        <main className="flex items-center justify-center">
             <div>aiueo</div>
         </main>
     );
