bce7298231b241f2692031173e9cfcb14cffea10
Azureit <d.f.18th@gmail.com>
2024-10-19 17:18:49 +0900

    c

--- src/components/org/c.tsx
+++ src/components/org/c.tsx
@@ -7,7 +7,7 @@ const Center = ({children}: {children: React.ReactNode}) => {
     // justify-center = horizontal
     return (
         <main className="flex items-center justify-center h-screen">
-            <div className={"p-10 bg-white font-semibold text-2xl text-orange-400"}>{children}</div>
+            <div className={"p-10 rounded-xl bg-white font-semibold text-2xl text-orange-400"}>{children}</div>
         </main>
     )
 }
