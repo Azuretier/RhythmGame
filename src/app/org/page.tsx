7dca15451acda806e36ef3bc2b4f6fe7b43c4a42
Azureit <d.f.18th@gmail.com>
2024-10-19 17:26:03 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -2,12 +2,17 @@
 
 import Center from "@/components/org/c"
 import B from "@/components/org/button"
+import {FadeUpDiv, FadeUpStagger} from "@/components/animation"
 
 export default function gatiiku() {
     return (
-        <section>
-            <Center>Hello how are yo' doing bro</Center>
-            <B/>
-        </section>
+        <main>
+            <FadeUpStagger>
+                <FadeUpDiv>
+                    <Center>Hello how are yo' doing bro</Center>
+                    <B/>
+                </FadeUpDiv>
+            </FadeUpStagger> 
+        </main>
     )
 }
