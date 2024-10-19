79cf30892347a55c8d330692dae142d8bfac49b2
Azureit <d.f.18th@gmail.com>
2024-10-20 07:46:26 +0900

    c

--- src/app/810/page.tsx
+++ src/app/810/page.tsx
@@ -5,10 +5,12 @@ import {motion} from 'framer-motion'
 const Gatiiku = () => {
     return (
         <motion.div
-      className="box"
-      initial={{ opacity: 0, scale: 0 }}
-      animate={{ opacity: 1, scale: 1 }}
-    />
+            className="box"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+        >
+            1919
+        </motion.div>
     )
 }
 
