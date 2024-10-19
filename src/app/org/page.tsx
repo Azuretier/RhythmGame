633b8b6720abe507a46e11f8b312e4e7e9de9e72
Azureit <d.f.18th@gmail.com>
2024-10-19 22:43:10 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -24,7 +24,7 @@ const Gatiiku = () => {
                         y:0,
                         transition: { 
                             type: 'spring',
-                            delay: 0.15
+                            delay: 1
                         }
                     }
                 }
