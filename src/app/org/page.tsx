1191327fcc571d68e3fc64ea0e5d6ea711d68318
Azureit <d.f.18th@gmail.com>
2024-10-22 04:35:06 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -33,7 +33,7 @@ const Main = () => {
                     <Image className="justify-self-center"src="/azure.png" alt="avatar" width={200} height={200}/>
                     <p className="text-3xl">Azuret</p>
                     <p className="text-subtext">あずれーと</p>
-                    <p className="font-normal text-subtext">世界は自分で創る</p>
+                    <p className="font-normal text-subtext text-lg">世界は自分で創る</p>
                 </section>
                 <div className="grid grid-flow-row gap-1 text-subtext">
                     <div className="grid grid-flow-col gap-2 justify-start">
