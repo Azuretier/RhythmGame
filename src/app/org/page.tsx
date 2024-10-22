d030e92ebeeca98106d53a6e427c42600f202829
Azureit <d.f.18th@gmail.com>
2024-10-23 03:51:40 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -36,7 +36,7 @@ const Main = () => {
                     <p className="font-normal text-subtext text-lg">世界は自分で創る</p>
                 </div>
                 <div className="grid grid-flow-row gap-2 grid-rows-3 grid-cols-2 text-subtext justify-start">
-                    <div className="grid grid-rows-1 gap-2 justify-start">
+                    <div className="grid grid-flow-col gap-2 justify-start">
                         <FaBirthdayCake/>
                         <p>200X/2/18</p>  
                     </div>
