87d6865f0098ca34de9cc346c34c3b9aeb8cd83a
Azureit <d.f.18th@gmail.com>
2025-08-15 14:08:33 +0900

    c

--- src/components/RainEffect.tsx
+++ src/components/RainEffect.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export default function RainEffect() {
   const ref = useRef<HTMLDivElement>(null);
