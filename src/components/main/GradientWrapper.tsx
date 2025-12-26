5e38c55aff9769c66f61e1d76d1f9fd719d303c1
Azureit <d.f.18th@gmail.com>
2025-12-26 14:04:34 +0900

    adjusting gradientWrapper

--- src/components/main/GradientWrapper.tsx
+++ src/components/main/GradientWrapper.tsx
@@ -31,17 +31,10 @@ export default function GradientMotionLink({
     >
       {/* The Gradient Layer - Now also animated by Motion */}
       <motion.div
-        className={`absolute inset-0 bg-gradient-to-r ${gradient} ${rounded} opacity-0 group-hover:opacity-100`}
+        className={`absolute backdrop-blur-xl inset-1 bg-gradient-to-r ${gradient} ${rounded} opacity-0 group-hover:opacity-100`}
         transition={{ duration: 0.3 }}
       />
-
-      {/* The Content Layer */}
-      <div 
-        className="relative bg-white dark:bg-slate-900 h-full w-full rounded-[calc(var(--radius)-2px)]"
-        style={{ '--radius': '12px' } as React.CSSProperties}
-      >
         {children}
-      </div>
     </motion.a>
   );
 }
