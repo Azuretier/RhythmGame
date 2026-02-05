6980f6bc9c133ce3f0c1950850613ad2b2b03132
Azureit <d.f.18th@gmail.com>
2026-02-05 14:18:11 +0900

    fix in use-mobile.ts

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -3,23 +3,21 @@ import { useEffect, useState } from "react"
 export function useIsMobile() {
   const [isMobile, setIsMobile] = useState(false);
 
-  /* Storing user's device details in a variable*/
-  let details = navigator.userAgent;
+  useEffect(() => {
+    /* Storing user's device details in a variable */
+    const details = navigator.userAgent;
 
-  /* Creating a regular expression 
-  containing some mobile devices keywords 
-  to search it in details string*/
-  let regexp = /android|iphone|kindle|ipad/i;
+    /* Creating a regular expression 
+    containing some mobile devices keywords 
+    to search it in details string */
+    const regexp = /android|iphone|kindle|ipad/i;
 
-  /* Using test() method to search regexp in details
-  it returns boolean value*/
-  let isMobileDevice = regexp.test(details);
+    /* Using test() method to search regexp in details
+    it returns boolean value */
+    const isMobileDevice = regexp.test(details);
 
-  if (isMobileDevice) {
-    setIsMobile(true);
-  } else {
-    setIsMobile(false);
-  }
+    setIsMobile(isMobileDevice);
+  }, []);
 
   return isMobile;
 }
